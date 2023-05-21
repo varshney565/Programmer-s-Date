96a67a8abb06d2cb275162f5fcb515703cb46213
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-05-21 13:45:35 +0530

    Update T-P.cpp

--- 3rd-4th-year/T-P.cpp
+++ 3rd-4th-year/T-P.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void solve(){
+    int tp,ts,n;cin>>tp>>ts>>n;
+    vector<int> ps(n),pp(n);
+    for(int i = 0 ; i < n ; i++) cin>>pp[i];
+    for(int i = 0 ; i < n ; i++) cin>>ps[i];
+    int ans = INT_MAX;
+    for(int i = 0 ; i < n ; i++){
+        if(ps[i] <= ts){
+            continue;
+        }else{
+            ans = min(ans,(tp-pp[i]+ps[i]-ts-1)/(ps[i]-ts));
+        }
+    }
+    if(ans == INT_MAX) cout<<"-1\n";
+    else cout<<ans<<"\n";
+}
+
+int main() {
+    int t;cin>>t;
+    while(t--) solve();
+    return 0;
+} 
